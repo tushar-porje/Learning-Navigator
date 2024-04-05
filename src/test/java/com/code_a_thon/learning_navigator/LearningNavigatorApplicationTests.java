e602c57d3a8874a401813915b7f738452508d8d3
Tushar Porje <tusharporje13091999@gmail.com>
2024-04-05 23:18:21 +0530

    first commit: workspace setup

---
+++ src/test/java/com/code_a_thon/learning_navigator/LearningNavigatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code_a_thon.learning_navigator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningNavigatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
